798805b1504df3a2f313e651f6e6d5cdef3fca79
DmitryDK86 <rang.demon@gmail.com>
2021-11-10 10:29:02 +0300

    refactor 2

--- src/main/java/ru/ddk/googlechatbot/config/BotConfig.java
+++ src/main/java/ru/ddk/googlechatbot/config/BotConfig.java
@@ -1,17 +1,15 @@
 package ru.ddk.googlechatbot.config;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
 import java.util.Objects;
 
 @Configuration
@@ -38,14 +36,9 @@ public class BotConfig {
     private String responseTemplate;
 
     @Bean
-    public GoogleCredential credential() throws IOException {
-        return GoogleCredential.fromStream(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream(getCredentialsPathEnvProperty())))
-                .createScoped(Collections.singleton(getGoogleChatApiScope()));
-    }
-
-    @Bean
-    public HttpRequestFactory requestFactory() throws GeneralSecurityException, IOException {
-        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        return httpTransport.createRequestFactory(credential());
+    public HttpRequestFactory requestFactory() throws IOException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(Objects.requireNonNull(getClass().getResourceAsStream(getCredentialsPathEnvProperty())));
+        HttpCredentialsAdapter credentialsAdapter = new HttpCredentialsAdapter(credentials);
+        return new NetHttpTransport().createRequestFactory(credentialsAdapter);
     }
 }
